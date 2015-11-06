a47c0954d476bf6d47dd453c2d072d3c72448bd4
petrarka <geonerus@mail.ru>
2015-11-06 22:04:01 +0300

    Update qr.py

--- qr.py
+++ qr.py
@@ -6,11 +6,11 @@ def intToByteArray(integer):
         uint16_max = 65536
         if not integer <= uint16_max:
             integer = uint16_max
-        return bytearray([(integer << 8) >> 8, integer >> 8])
+        return bytearray([(integer >> 8) + 1, ((integer << 8) >> 8) + 1])
 
 a=[]
 f=0
-inp=input('vedi:')
+inp = input('vedi:')
 while len(inp)>0:
     if len(inp)<=798:
         a.append(inp)
@@ -31,16 +31,14 @@ while len(a)!=z:
     box_size=5,
     border=4,
     )
-    a[z]= bytearray(a[z], encoding='UTF-8')+intToByteArray(z+1)
+    a[z] = bytearray(a[z], encoding='UTF-8')+intToByteArray(z+1)
     print(a[z])
-    a[z]=a[z].decode('UTF-8')
+    a[z] = a[z].decode('UTF-8')
     print(a[z])
     qr.add_data(a[z])
     qr.make(fit=True)
-
+    dirp=raw_inpit('Enter save directory: ')
     img = qr.make_image()
-    img.save('qr'+str(z)+'.png')
+    img.save(dirp+'qr'+str(z)+'.png', )
     qr=''
     z=z+1
-
-
